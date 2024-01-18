225c2291ea19f6c133fdff0ed4d48ff7c283be63
Poopbender21 <gustapes@student.gu.se>
2024-01-18 14:50:45 +0100

    add interface

--- Car.java
+++ Car.java
@@ -1,11 +1,17 @@
 import java.awt.*;
 
-public class Car{
+public class Car implements Movable{
     private final int nrDoors; // Number of doors on the car
     protected final double enginePower; // Engine power of the car
     protected double currentSpeed; // The current speed of the car
     private Color color; // Color of the car
     private final String modelName; // The car model name
+
+    //movement saker
+    private int direction;
+    private double Xpos;
+    private double Ypos;
+
 
     public Car(int nrDoor, double enginePower, Color color, String modelName){
         this.nrDoors = nrDoor;
@@ -14,6 +20,11 @@ public class Car{
         this.modelName = modelName;
         currentSpeed = 0;
         stopEngine();
+
+        // movement saker
+        this.direction = 1;
+        this.Xpos = 0;
+        this.Ypos = 0;
     }
 
     public int getNrDoors(){
@@ -42,9 +53,11 @@ public class Car{
     public void stopEngine(){
         currentSpeed = 0;
     }
+
     public double speedFactor(){
         return enginePower * 0.01;
     }
+
     public void incrementSpeed(double amount){
         currentSpeed += amount;
     }
@@ -53,4 +66,33 @@ public class Car{
         currentSpeed -= amount;
     }
 
+    // movement saker
+    @Override
+    public void move() {
+        if (direction == 1){
+            this.Ypos += currentSpeed;
+        }
+        if (direction == 3){
+            this.Ypos -= currentSpeed;
+        }
+        if (direction == 2){
+            this.Xpos += currentSpeed;
+        }
+        if (direction ==4){
+            this.Xpos -= currentSpeed;
+        }
+    }
+
+    @Override
+    public void turnLeft() {
+        direction = (direction + 1) % 4;
+    }
+
+    @Override
+    public void turnRight() {
+        direction--;
+        if (direction < 1){
+            direction = 4;
+        }
+    }
 }
