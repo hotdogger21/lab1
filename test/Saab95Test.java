16557f74e7bf1d722db38f9bd659dbfca8636a97
Poopbender21 <gustapes@student.gu.se>
2024-01-18 16:37:07 +0100

    add more test

--- test/Saab95Test.java
+++ test/Saab95Test.java
@@ -1,8 +1,9 @@
 import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
-public class Saab95Test extends TestCase {
+public class Saab95Test {
     Saab95 car;
 
     @Before
@@ -10,29 +11,42 @@ public class Saab95Test extends TestCase {
         car = new Saab95();
     }
 
+    @Test
     public void testSetTurboOn() {
         car.setTurboOn();
         Assert.assertTrue(car.getTurbo());
     }
 
+    @Test
     public void testSetTurboOff() {
         car.setTurboOff();
         Assert.assertFalse(car.getTurbo());
     }
 
+    @Test
     public void testSpeedFactor() {
-
+        Assert.assertEquals(1.25, car.speedFactor(), 1e-15);
     }
 
+    @Test
     public void testIncrementSpeed() {
+        car.startEngine();
+        car.incrementSpeed(1);
+        Assert.assertTrue(car.getCurrentSpeed() > 0.1);
     }
 
+    @Test
     public void testDecrementSpeed() {
+        car.startEngine();
+        car.decrementSpeed(1);
+        Assert.assertTrue(car.getCurrentSpeed() < 0.1);
     }
 
+    @Test
     public void testGas() {
     }
 
+    @Test
     public void testBrake() {
     }
 }
--- test/Volvo240Test.java
+++ test/Volvo240Test.java
@@ -1,28 +1,35 @@
 import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
-public class Volvo240Test extends TestCase {
+public class Volvo240Test {
 
-    public void testSpeedFactor() {
+    Volvo240 car;
+
+    @Before
+    public void init(){
+        car = new Volvo240();
     }
 
+    @Test
+    public void testSpeedFactor() {
+        Assert.assertEquals(1.25, car.speedFactor(), 1e-15);
+    }
+
+    @Test
     public void testIncrementSpeed() {
     }
 
+    @Test
     public void testDecrementSpeed() {
     }
 
-    public void testTestSpeedFactor() {
-    }
-
-    public void testTestIncrementSpeed() {
-    }
-
-    public void testTestDecrementSpeed() {
-    }
-
+    @Test
     public void testGas() {
     }
 
+    @Test
     public void testBrake() {
     }
 }
